d0e737ad444c009f792dae0c6821a4d7badfdb8d
Morgan Spencer <morgan@spencercreative.co>
2021-04-29 18:39:21 -0400

    Add homepage

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -1,18 +1,13 @@
-// In this file, all Page components from 'src/pages` are auto-imported. Nested
-// directories are supported, and should be uppercase. Each subdirectory will be
-// prepended onto the component name.
-//
-// Examples:
-//
-// 'src/pages/HomePage/HomePage.js'         -> HomePage
-// 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
-
-import { Router, Route } from '@redwoodjs/router'
+import { Router, Route, Set } from '@redwoodjs/router'
+import AdminLayout from 'src/layouts/AdminLayout'
 
 const Routes = () => {
   return (
     <Router>
-      <Route path="/dashboard" page={DashboardPage} name="dashboard" />
+      <Route path="/" page={HomePage} name="home" />
+      <Set wrap={AdminLayout}>
+        <Route path="/dashboard" page={DashboardPage} name="dashboard" />
+      </Set>
       <Route notfound page={NotFoundPage} />
     </Router>
   )
