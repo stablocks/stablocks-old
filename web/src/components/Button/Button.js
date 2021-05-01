50b9ddea1831c5f608c48d81f1997ed5fa13cb1b
Morgan Spencer <morgan@spencercreative.co>
2021-04-30 23:53:21 -0400

    Add button component

---
+++ web/src/components/Button/Button.js
@@ -0,0 +1,23 @@
+const Button = (props) => {
+  return (
+    <props.as
+      to={props?.to}
+      onClick={props?.onClick}
+      className={`${
+        props.main
+          ? 'border-transparent text-white bg-indigo-500 hover:bg-indigo-600'
+          : 'border-gray-300 text-gray-700 bg-white hover:bg-gray-50'
+      } inline-flex items-center px-4 py-2 border rounded-md shadow-sm text-sm font-medium transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
+    >
+      {props.icon && (
+        <props.icon
+          className={`${!props.main ? 'text-gray-500' : ''} -ml-1 mr-2 h-5 w-5`}
+          aria-hidden="true"
+        />
+      )}
+      {props.title}
+    </props.as>
+  )
+}
+
+export default Button
