bbb4539e76e291eeac446d86f5a5a6f95fbce94e
Morgan Spencer <morgan@spencercreative.co>
2021-05-02 22:11:57 -0400

    Update login page

--- web/src/pages/LoginPage/LoginPage.js
+++ web/src/pages/LoginPage/LoginPage.js
@@ -1,14 +1,18 @@
+import { useState } from 'react'
 import { Link, navigate, routes } from '@redwoodjs/router'
 import { useAuth } from '@redwoodjs/auth'
 import { Form, Label, TextField, FieldError, Submit } from '@redwoodjs/forms'
 
 const LoginPage = () => {
+  const [linkSent, setLinkSent] = useState(false)
+  const [email, setEmail] = useState('')
+
   const { logIn, currentUser } = useAuth()
   const onSubmit = (data) => {
     logIn({
       email: data.email,
     }).then(() => {
-      navigate(routes.home())
+      setLinkSent(true)
     })
   }
 
@@ -31,37 +35,45 @@ const LoginPage = () => {
 
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <Form onSubmit={onSubmit} className="space-y-6">
-            <div>
-              <Label
-                name="email"
-                className="block text-sm font-medium text-gray-700"
-                errorClassName="block text-sm font-medium text-red-700"
-              >
-                Email Address
-              </Label>
-              <div className="mt-1">
-                <TextField
+          {linkSent ? (
+            <p className="text-center">
+              Login link sent to <em>{email}</em>
+            </p>
+          ) : (
+            <Form onSubmit={onSubmit} className="space-y-6">
+              <div>
+                <Label
                   name="email"
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                  errorClassName="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                  validation={{
-                    required: true,
-                    pattern: {
-                      value: /[^@]+@[^\.]+\..+/,
-                    },
-                  }}
-                />
+                  className="block text-sm font-medium text-gray-700"
+                  errorClassName="block text-sm font-medium text-red-700"
+                >
+                  Email Address
+                </Label>
+                <div className="mt-1">
+                  <TextField
+                    name="email"
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    errorClassName="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    validation={{
+                      required: true,
+                      pattern: {
+                        value: /[^@]+@[^\.]+\..+/,
+                      },
+                    }}
+                  />
+                </div>
+                <FieldError name="email" className="error-message" />
               </div>
-              <FieldError name="email" className="error-message" />
-            </div>
 
-            <div>
-              <Submit className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                Send sign in link to email
-              </Submit>
-            </div>
-          </Form>
+              <div>
+                <Submit className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-400 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                  Send sign in link to email
+                </Submit>
+              </div>
+            </Form>
+          )}
         </div>
       </div>
     </div>
