3b5710eef135cd4c130230b61ec80cbe5aad317e
Morgan Spencer <morgan@spencercreative.co>
2021-05-05 08:27:23 -0400

    Add logout button to profile page

--- web/src/pages/ProfilePage/ProfilePage.js
+++ web/src/pages/ProfilePage/ProfilePage.js
@@ -1,11 +1,16 @@
 import { Link, routes } from '@redwoodjs/router'
+import { useAuth } from '@redwoodjs/auth'
 import DashboardHeading from 'src/components/DashboardHeading'
+import Button from 'src/components/Button'
 
 const ProfilePage = () => {
+  const { logOut } = useAuth()
   const title = 'Profile'
   return (
     <>
-      <DashboardHeading title={title} />
+      <DashboardHeading title={title}>
+        <Button title="Logout" as="button" onClick={logOut} />
+      </DashboardHeading>
       <p>
         Find me in <code>./web/src/pages/ProfilePage/ProfilePage.js</code>
       </p>
