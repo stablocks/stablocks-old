efc5b0959f32f944cfe1b1ca8550e57bb2efa3c1
Morgan Spencer <morgan@spencercreative.co>
2021-05-26 10:08:08 -0400

    Update routes with new pages

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -9,17 +9,26 @@ const Routes = () => {
     <Router>
       <Private unauthenticated="login">
         <Set wrap={[OrganizationProviderCell, AdminLayout]}>
+          <Route path="/organization/settings" page={SettingsPage} name="settings" />
+          <Route path="/organization/departments" page={DepartmentsPage} name="departments" />
+          <Route path="/organization/employees" page={EmployeesPage} name="employees" />
+          <Route path="/organization" page={OrganizationPage} name="organization" />
+          <Route path="/recruit/applications" page={ApplicationsPage} name="applications" />
+          <Route path="/recruit/jobs" page={JobsPage} name="jobs" />
+          <Route path="/recruit" page={RecruitPage} name="recruit" />
+          <Route path="/projects/tasks" page={TasksPage} name="tasks" />
+          <Route path="/projects" page={ProjectsPage} name="projects" />
           <Route path="/helpdesk/tickets" page={TicketsPage} name="tickets" />
           <Route path="/helpdesk" page={HelpdeskPage} name="helpdesk" />
           <Route path="/profile" page={ProfilePage} name="profile" />
           <Route path="/dashboard" page={DashboardPage} name="dashboard" />
         </Set>
       </Private>
+      <Set wrap={FormLayout}>
+        <Route path="/signup" page={SignupPage} name="signup" />
+        <Route path="/login" page={LoginPage} name="login" />
+      </Set>
       <Set wrap={MainLayout}>
-        <Set wrap={FormLayout}>
-          <Route path="/signup" page={SignupPage} name="signup" />
-          <Route path="/login" page={LoginPage} name="login" />
-        </Set>
         <Route path="/" page={HomePage} name="home" />
         <Route notfound page={NotFoundPage} />
       </Set>
